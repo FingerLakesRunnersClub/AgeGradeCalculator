793b5e6d23b5693861c31cfe13880aef39368628
Steve Desmond <steve@vtsv.ca>
2020-12-12 12:52:55 -0500

    Update for NuGet package consumption

--- AgeGradeCalculator/Records.cs
+++ AgeGradeCalculator/Records.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace FLRC.AgeGradeCalculator
 {
-    public class Records
+    public static class Records
     {
         public static IDictionary<Identifier, uint> All = new Dictionary<Identifier, uint>
         {
